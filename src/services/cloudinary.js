bca9beae46730c2ec978c187e034388c2bc64264
Moiz Aleem <mianmoiz0000@gmail.com>
2025-10-23 22:41:19 +0500

    admin

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -1,28 +1,4 @@
-// src/services/cloudinary.js
-// export const uploadImageToCloudinary = async (file) => {
-//   const cloudName = "dnyiq1pbt"; // 🔹 Replace with your Cloudinary cloud name
-//   const uploadPreset = "ml_default"; // 🔹 Replace with your unsigned preset
 
-//   const formData = new FormData();
-//   formData.append("file", file);
-//   formData.append("upload_preset", uploadPreset);
-
-//   try {
-//     const res = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
-//       method: "POST",
-//       body: formData,
-//     });
-
-//     const data = await res.json();
-//     return data.secure_url; // ✅ This is the URL to store in Firestore
-//   } catch (error) {
-//     console.error("Cloudinary upload error:", error);
-//     throw error;
-//   }
-// };
-
-
-// ------------------------------------ client
 
 
 export const uploadImageToCloudinary = async (file) => {
@@ -49,24 +25,3 @@ export const uploadImageToCloudinary = async (file) => {
 };
 
 
-// export const uploadPDFToCloudinary = async (file) => {
-//   const cloudName = "huzaimaikram"; // Replace with your Cloudinary cloud name
-//   const uploadPreset = "agency_data"; // Unsigned preset
-
-//   const formData = new FormData();
-//   formData.append("file", file);
-//   formData.append("upload_preset", uploadPreset);
-
-//   try {
-//     const res = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/raw/upload`, {
-//       method: "POST",
-//       body: formData,
-//     });
-
-//     const data = await res.json();
-//     return data.secure_url; // ✅ URL to store in Firestore pdf array
-//   } catch (error) {
-//     console.error("Cloudinary PDF upload error:", error);
-//     throw error;
-//   }
-// };
